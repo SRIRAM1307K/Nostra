1f8179ded0f992de27f4540328c88a6a494f097f
SRIRAM1307K <sriramkarthi2000@gmail.com>
2025-04-23 18:26:21 +0530

    sideNav

---
+++ collection.js
@@ -0,0 +1,18 @@
+// Menu Bar Slider 
+
+let menuBar = document.getElementById("side-navbar-activate")
+let closeBtn = document.getElementById("rows")
+let sideContent = document.getElementById("sideContent")
+
+menuBar.addEventListener("click", function(){
+    sideContent.style.left="0%"
+})
+closeBtn.addEventListener("click", function(){
+    sideContent.style.left="-50%"
+})
+
+let crossBtn = document.getElementById("crossBtn")
+let offerBar = document.getElementById("top")
+crossBtn.addEventListener("click",function(){
+    offerBar.style.display="none"
+})
